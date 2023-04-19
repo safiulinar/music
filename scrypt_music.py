8a5c09658bad7f4678420be2bb773142ece4aca8
safiulinar <safiulinar@yandex.ru>
2023-04-19 10:33:32 +0300

    Разбор словаря

--- scrypt_music.py
+++ scrypt_music.py
@@ -3,7 +3,9 @@ sys.path.append(r"\DEV\music\lib")
 from parsing_music import get_artist_col
 
 dict_music = {}
-
 dict_music = get_artist_col("ц")
 
-print(dict_music)
+for l in dict_music.keys():
+    for n in dict_music[l].keys():
+        for m in range(len(dict_music[l][n])):
+            print(dict_music[l][n][m])
