b05e6f4b6b4d1e294f4e9bf1fa3fc34856dadbd9
safiulinar <safiulinar@yandex.ru>
2023-04-20 07:12:17 +0300

    2

--- scrypt_music.py
+++ scrypt_music.py
@@ -3,9 +3,12 @@ sys.path.append(r"\DEV\music\lib")
 from parsing_music import get_artist_col
 
 dict_music = {}
-dict_music = get_artist_col("ц")
+dict_music = get_artist_col("я")
 
-for l in dict_music.keys():
-    for n in dict_music[l].keys():
-        for m in range(len(dict_music[l][n])):
-            print(dict_music[l][n][m])
+print(dict_music)
+
+#for l in dict_music.keys():
+#    print(l)
+#    for n in dict_music[l].keys():
+#        for m in range(len(dict_music[l][n])):
+#            print(dict_music[l][n][m])
